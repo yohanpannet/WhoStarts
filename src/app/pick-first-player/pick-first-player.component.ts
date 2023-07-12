1dfcbd1163502ff0a8edde7913739eb97be64c83
yohanpannet <yohan.pannet@gmail.com>
2023-07-12 18:25:35 +0200

    touches

--- src/app/pick-first-player/pick-first-player.component.ts
+++ src/app/pick-first-player/pick-first-player.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-pick-first-player',
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./pick-first-player.component.scss']
 })
 export class PickFirstPlayerComponent {
+  @ViewChild('canvas', { static: true })
+  canvas!: ElementRef<HTMLCanvasElement>;
+  @HostListener('touchstart', ['$event'])
+  handleTouch(event:TouchEvent){
+    console.log(`%c ${event.type}`, 'color: pink');
+    console.log(event)
+    if (!this.ctx) return;
+    for (var i = 0; i < event.touches.length; i++) {
+      var touch = event.touches[i];
+      this.ctx.beginPath();
+      this.ctx.arc(touch.pageX, touch.pageY, 20, 0, 2* Math.PI, true);
+      this.ctx.fill();
+      this.ctx.stroke();
+    }
+  }
+  @HostListener('touchmove', ['$event'])
+  handleTouchMove(event:TouchEvent){
+    console.log(`%c ${event.type}`, 'color: lime');
+    console.log(event)
+  }
+  @HostListener('touchend', ['$event'])
+  handleTouchEnd(event:TouchEvent){
+    console.log(`%c ${event.type}`, 'color: orange');
+    console.log(event)
+  }
 
+  private ctx: CanvasRenderingContext2D | null = null;
+
+  ngOnInit(): void {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+  }
+  
 }
