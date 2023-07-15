fe2ac95c1ae50016e09897d9c73ca25df5b1f4fd
yohanpannet <yohan.pannet@gmail.com>
2023-07-15 11:15:53 +0200

    countdown

--- src/app/pick-first-player/pick-first-player.component.ts
+++ src/app/pick-first-player/pick-first-player.component.ts
@@ -4,13 +4,15 @@ const colorPalettes: Map<string, string[]> = new Map([
   ['rainbowdash', ["#ee4035", "#f37736", "#fdf498", "#7bc043",  "#0392cf"]],
   ['google', ["#008744","#0057e7","#d62d20","#ffa700","#ffffff"]],
   ['metroUI', ["#d11141", "#00b159", "#00aedb", "#f37735", "#ffc425"]],
-  ['programCatalog', ["#edc951", "#eb6841", "#cc2a36", "#4f372d", "#00a0b0"]]
+  //['programCatalog', ["#edc951", "#eb6841", "#cc2a36", "#4f372d", "#00a0b0"]]
 ])
 
 interface TouchesTemplate {
   identifier: number,
   color:string,
 }
+
+const COUNT_DOWN_LENGTH = 3;
 
 @Component({
   selector: 'app-pick-first-player',
@@ -24,6 +26,9 @@ export class PickFirstPlayerComponent {
   width = 0;
   height = 0;
   paletteName= "";
+
+  countDown = 5;
+  countDownInterval: number;
 
   private ctx: CanvasRenderingContext2D | null = null;
   private toucheTemplates: TouchesTemplate[] = [];
@@ -54,7 +59,8 @@ export class PickFirstPlayerComponent {
   @HostListener('touchstart', ['$event'])
   @HostListener('touchmove', ['$event'])
   handleTouch(event:TouchEvent){
-    if (event.type ==='touchstart') {
+    if (event.type ==='touchstart' && event.touches.length > 1) {
+      this.startCountDown();
     }
     this.touchList = event.touches;
     //this.drawTouches(event.touches);
@@ -62,9 +68,12 @@ export class PickFirstPlayerComponent {
 
   @HostListener('touchend', ['$event'])
   handleTouchEnd(event:TouchEvent){
-    console.log(`%c ${event.type}`, 'color: orange');
-    console.log(event)
     this.removeTouch(event.changedTouches);
+    if (event.touches.length <= 1) {
+      this.resetCountDown()
+    } else {
+      this.startCountDown();
+    }
   }
 
   private removeTouch(touchList: TouchList) {
@@ -81,22 +90,56 @@ export class PickFirstPlayerComponent {
   private drawTouches(touchList: TouchList) {
 
     if (!this.ctx) return;
-    //this.ctx.clearRect(0, 0, this.width, this.height);
-    for (var i = 0; i < touchList.length; i++) {
-      var touch = touchList[i];
-      //get color
-      let touchTemplate = this.toucheTemplates[touch.identifier]
-      let color = touchTemplate ? touchTemplate.color : '#000000';
-
-      this.ctx.beginPath();
-      this.ctx.arc(touch.pageX, touch.pageY, 50, 0, 2* Math.PI, true);
-      this.ctx.fillStyle = color;
-
-      this.ctx.fill();
-      this.ctx.lineWidth = 2.0;
-		  this.ctx.strokeStyle = "rgba(0, 0, 200, 0.8)";
-      this.ctx.stroke();
+    this.ctx.clearRect(0, 0, this.width, this.height);
+    if (this.firstPlayerTouch) {
+      this.drawTouch(this.firstPlayerTouch);
+    } else {
+      for (var i = 0; i < touchList.length; i++) {
+        var touch = touchList[i];
+        this.drawTouch(touch);
+      }
     }
   }
   
+  private drawTouch(touch: Touch) {
+    //get color
+    let touchTemplate = this.toucheTemplates[touch.identifier]
+    let color = touchTemplate ? touchTemplate.color : '#000000';
+
+    this.ctx.beginPath();
+    this.ctx.arc(touch.pageX, touch.pageY, 50, 0, 2* Math.PI, true);
+    this.ctx.fillStyle = color;
+
+    this.ctx.fill();
+    this.ctx.lineWidth = 2.0;
+    this.ctx.strokeStyle = "rgba(0, 0, 200, 0.8)";
+    this.ctx.stroke();
+  }
+
+  private startCountDown() {
+    if (this.countDownInterval) {
+      clearInterval(this.countDownInterval);
+    }
+    this.countDown = COUNT_DOWN_LENGTH;
+    this.countDownInterval = window.setInterval(() => {
+      this.countDown --;
+      if (this.countDown ===0) {
+        clearInterval(this.countDownInterval);
+        this.pickFirstPlayer();
+      }
+    }, 1000)
+  }
+
+  private resetCountDown() {
+    if (this.countDownInterval) {
+      clearInterval(this.countDownInterval);
+    }
+    this.countDownInterval = undefined;
+  }
+
+  firstPlayerTouch: Touch;
+  private pickFirstPlayer() {
+    let firstPlayerIndex = Math.floor(Math.random() * this.touchList.length);
+    this.firstPlayerTouch = this.touchList[firstPlayerIndex];
+  }
 }
